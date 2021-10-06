d4fc36fec5cad1b07997350b3b37022fa5b69d23
Nick Lindau <nicholaithegreat@gmail.com>
2021-10-05 18:07:58 -0700

    add notes for promises

--- 4-callbacks.js
+++ 4-callbacks.js
@@ -1,44 +1,59 @@
-setTimeout(() => {
-  console.log('Two seconds are up');
-}, 2000);
+// setTimeout(() => {
+//   console.log('Two seconds are up');
+// }, 2000);
 
-const names = ['Andrew', 'Jen', 'Jess']
-const shortNames = names.filter((name) => {
-  return name.length <= 4
-})
+// const names = ['Andrew', 'Jen', 'Jess']
+// const shortNames = names.filter((name) => {
+//   return name.length <= 4
+// })
 
-console.log(shortNames);
+// console.log(shortNames);
 
-const geocode = (address, callback) => {
-  setTimeout(() => {
-    const data = {
-      latitude: 0,
-      longitude: 0
-    }
+// const geocode = (address, callback) => {
+//   setTimeout(() => {
+//     const data = {
+//       latitude: 0,
+//       longitude: 0
+//     }
   
-    callback(data)
-  }, 2000);
-}
+//     callback(data)
+//   }, 2000);
+// }
 
-geocode('Philadelphia', (data) => {
-  console.log(data);
-})
+// geocode('Philadelphia', (data) => {
+//   console.log(data);
+// })
 
-//
-// Goal: Mess around with the callback pattern
-//
-// 1. Define an add function that accepts the correct arguments
-// 2. Use setTimeout to simulate a 2 second delay
-// 3. After 2 seconds are up, call the callback function with the sum
-// 4. Test your work!
-const add = (x, y, callback) => {
+// //
+// // Goal: Mess around with the callback pattern
+// //
+// // 1. Define an add function that accepts the correct arguments
+// // 2. Use setTimeout to simulate a 2 second delay
+// // 3. After 2 seconds are up, call the callback function with the sum
+// // 4. Test your work!
+// const add = (x, y, callback) => {
+//   setTimeout(() => {
+//     const sum = x + y
+//     callback(sum)
+//   }, 2000);
+// }
+
+
+// add(1, 4, (sum) => {
+//   console.log(sum) // Should print: 5
+// })
+
+//! ~~~~~~~~~~ Examples to convert to Promises ~~~~~~~
+
+const doWorkCallback = (callback) => {
   setTimeout(() => {
-    const sum = x + y
-    callback(sum)
+    // callback('This is my error!', undefined);
+    callback(undefined, [1, 4, 7])
   }, 2000);
-}
+};
 
+doWorkCallback((error, result) => {
+  if(error) return console.log(error);
 
-add(1, 4, (sum) => {
-  console.log(sum) // Should print: 5
-})
+  console.log(result);
+});
